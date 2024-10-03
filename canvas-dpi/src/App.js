a13a3cf33e99025f1b0a467fb72bd160bd89ba0f
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-10-03 15:59:13 +0300

    icons and bg

--- canvas-dpi/src/App.js
+++ canvas-dpi/src/App.js
@@ -7,22 +7,28 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className="bg-blue-100 min-h-screen">
-      <header className="bg-blue-500 text-white py-6">
-        <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-2xl">Online DPI Converter</h1>
-          <nav>
-            <ul className="flex space-x-4">
-              <li><a href="/" className="hover:underline">Home</a></li>
-              <li><a href="/" className="hover:underline">English ▼</a></li>
-            </ul>
-          </nav>
-        </div>
+    <div className="flex flex-col min-h-screen">
+      <div className='bg-blue-500 p-4'>
+      <header className="text-white flex justify-between text-center py-4 w-full">
+        <h1 className="text-2xl">Online DPI Converter</h1>
+        <nav>
+          <ul className="flex justify-center space-x-4 mt-2">
+            <li><a href="/" className="hover:underline">Home</a></li>
+            <li><a href="/" className="hover:underline">English ▼</a></li>
+          </ul>
+        </nav>
       </header>
-      <div className="mx-auto mt-0">
+      </div>
+    <div className="flex-1 bg-blue-500 flex items-end justify-center">
+      
+    </div>
+    <div className="flex-1 bg-white flex items-start justify-center">
+      <div className="w-full max-w-6xl px-4 ml-40 -mt-44"> 
+        <ToastContainer />
         <Uploader />
       </div>
     </div>
+  </div>
   );
 }
 
--- canvas-dpi/src/components/DPI/Uploader.js
+++ canvas-dpi/src/components/DPI/Uploader.js
@@ -1,10 +1,12 @@
 import React, { useState, useCallback, useRef } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { changeDpiDataUrl } from 'changedpi';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan, faCloudArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 function Uploader() {
   const [imageSrc, setImageSrc] = useState('');
-  const [selectedDpi, setSelectedDpi] = useState(72); // Default DPI
+  const [selectedDpi, setSelectedDpi] = useState('');
   const canvasRef = useRef(null);
 
   const dpis = [72, 150, 200, 300, 400, 600];
@@ -53,46 +55,54 @@ function Uploader() {
   };
 
   return (
-    <div className="p-4 bg-white shadow rounded-lg">
-      <canvas ref={canvasRef} className="hidden" />
-      <div {...getRootProps()} className="dropzone border-dashed border-2 h-60 border-gray-400 rounded-lg p-10 flex flex-col items-center justify-center cursor-pointer">
-        <input {...getInputProps()} />
-        {isDragActive ?
-          <p>Drop the files here ...</p> :
-          imageSrc ? <img src={imageSrc} alt="Preview" className="max-w-full max-h-52 mt-4" /> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-        }
-      </div>
-      <div></div>
-      <div className="flex mt-4 justify-center">
-        {dpis.map((dpi) => (
-          <button
-            key={dpi}
-            onClick={() => setSelectedDpi(dpi)}
-            className={`py-2 px-8 border ${selectedDpi === dpi ? 'border-blue-500 text-blue-500' : 'border-gray-300 text-gray-700 hover:border-blue-500 hover:text-blue-500'} rounded transition-colors`}
-          >
-            {dpi}
-          </button>
-        ))}
-        <input
-          type="text"
-          placeholder="Custom"
-          className="rounded border px-4 py-2 hover:border-blue-500 hover:text-blue-500"
-          value={selectedDpi}
-          onChange={(e) => setSelectedDpi(Number(e.target.value))}
-        />
-      </div>
-      <div className="mt-4">
-        <button onClick={deleteImage} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-          Delete Image
-        </button>
-      </div>
-      <div className="flex space-x-4 mt-4">
-        <button onClick={handleDownloadClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Download Image with New DPI
-        </button>
-      </div>
+    <div className="p-4 bg-white shadow justify-center rounded-lg w-9/12 mt-6">
+       <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
+      <div>
+    <div {...getRootProps()} className="dropzone border-dashed border-2 h-60 border-gray-400 rounded-lg p-16 flex flex-col items-center justify-center cursor-pointer">
+      <input {...getInputProps()} />
+      {isDragActive ?
+        <p>Drop the files here ...</p> :
+        imageSrc ? <img src={imageSrc} alt="Preview" className="max-w-full max-h-48 mt-4" /> :
+        <div>
+          <img src="https://www.dpi-converter.com/assets/images/file-upload.png" alt="Placeholder" className="max-w-full max-h-48 mt-4" />
+          <p className='mt-4 mb-4'>Drag 'n' drop some files here, or click to select files</p>
+        </div>
+      }
     </div>
+    <div className='flex justify-between'>
+    <div className="flex mt-4 justify-center ">
+      {dpis.map((dpi) => (
+        <button
+          key={dpi}
+          onClick={() => setSelectedDpi(dpi)}
+          className={` w-20 border  ${selectedDpi === dpi ? 'border-blue-500 text-blue-500' : 'border-gray-300 text-gray-400 hover:border-blue-500 hover:text-blue-500'}  transition-colors`}
+        >
+          {dpi}
+        </button>
+      ))}
+      <input
+        type="text"
+        placeholder="Custom"
+        className="w-24 border border-gray-300 px-4 py-2 hover:border-blue-500 hover:text-blue-500"
+        value={selectedDpi}
+        onChange={(e) => setSelectedDpi(Number(e.target.value))}
+      />
+    </div>
+    <div className='flex'>
+    <div className="mt-4">
+      <button onClick={deleteImage} className="bg-black hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+      <FontAwesomeIcon className='' icon={faTrashCan} />
+      </button>
+    </div>
+    <div className="flex space-x-4 mt-4">
+      <button onClick={handleDownloadClick} className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <FontAwesomeIcon icon={faCloudArrowDown} />
+      </button>
+    </div>
+    </div>
+    </div>
+    </div>
+  </div>
   );
 }
 
