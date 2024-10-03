83c9cdd0f2eed8c42e199465328829cfd3ab70f3
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-10-03 09:28:36 +0300

    convert button

--- canvas-dpi/src/App.js
+++ canvas-dpi/src/App.js
@@ -7,7 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-      <div className="bg-blue-100 min-h-screen">
+      <div className="bg-blue-100 min-h-screen ">
           <header className="bg-blue-500 text-white py-32">
               <div className="container mx-auto flex justify-between items-center">
                   <h1 className="text-xl md:text-2xl">Online DPI Converter</h1>
--- canvas-dpi/src/components/DPI/Uploader.js
+++ canvas-dpi/src/components/DPI/Uploader.js
@@ -53,13 +53,20 @@ function Uploader() {
         </>
         )}  
         </div>
-        <div className="flex justify-center space-x-4 mt-4">
+        <div className='justify-between flex'>
+          <div className="flex justify-center space-x-4 mt-4">
             {[72, 150, 200, 300, 400, 600].map(dpi => (
                 <button key={dpi} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     {dpi}
                 </button>
             ))}
             <input placeholder='Custom' className="bg-gray-300 hover:bg-gray-400 text-black py-2 px-2 rounded"/>
+          </div>
+          <div className="flex justify-center mt-4">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Convert
+            </button>
+          </div>
         </div>
     </div>
 );
