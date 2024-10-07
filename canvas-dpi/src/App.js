a5682ea5be038931cd7732d2b1d5511ec3f0447d
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-10-07 09:40:23 +0300

    blue background moved lower

--- canvas-dpi/src/App.js
+++ canvas-dpi/src/App.js
@@ -9,10 +9,10 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <div className='bg-blue-500 p-4' id='Aa'>
-        <header className="text-white 2xl:flex justify-between text-center py-4 w-full" id='header' >
+        <header className="text-white 2xl:flex justify-between text-center py-4 w-full mb-80" id='header' >
           <h1 className="text-2xl">Online DPI Converter</h1>
           <nav>
-            <ul className="flex justify-center space-x-4 mt-2">
+            <ul className="flex justify-center space-x-4">
               <li><a href="/" className="hover:underline">Home</a></li>
               <li><a href="/" className="hover:underline">English ▼</a></li>
             </ul>
@@ -22,7 +22,7 @@ function App() {
     <div className="flex-1 bg-blue-500 flex items-end justify-center">
     </div>
     <div className="flex-1 bg-white flex items-start justify-center">
-      <div className="w-full max-w-7xl px-4 2xl:ml-64 2xl:-mt-44">
+      <div className="w-full max-w-7xl px-4 2xl:ml-64 2xl:-mt-96">
           <p className="text-3xl 2xl:mt-16 2xl:ml-80 text-white" id='p1'>Online DPI Converter</p>
             <p className='text-center text-white mr-72' id='p2'>Batch modify DPI of images, supports various formats such as JPG, PNG, GIF, etc.</p> 
         <ToastContainer />
